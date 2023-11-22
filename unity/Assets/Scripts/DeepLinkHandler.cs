7b4bf2720c1ef16be82cf975402674e8bf6b66bf
Alex <gounisalex@gmail.com>
2023-11-22 01:10:02 +0100

    implements SignalR [iOS compatible]

--- unity/Assets/Scripts/DeepLinkHandler.cs
+++ unity/Assets/Scripts/DeepLinkHandler.cs
@@ -1,19 +1,36 @@
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+[System.Serializable]
+public class AddressObject
+{
+    public string[] addresses;
+}
+
 
 public class DeepLinkHandler : MonoBehaviour
 {
     public Text textfield;
     public Button signInButton;
     public Button sendOperation;
+    public string token;
 
     public static DeepLinkHandler Instance { get; private set; }
     public string deeplinkURL;
 
-    void Start() {
+    private HubConnection connection;
+
+    async void Start() {
         textfield.gameObject.SetActive(false);
         sendOperation.gameObject.SetActive(false);
+        await GetConnection();
     }
 
     private void Awake()
@@ -49,4 +66,24 @@ public class DeepLinkHandler : MonoBehaviour
         textfield.text = "Address: " +  address.Substring(0, 4) + "..." + address.Substring(address.Length - 4);
         WebViewController.buyURL = $"https://global.transak.com/?apiKey=f1336570-699b-4181-9bd1-cdd57206981f&cryptoCurrencyCode=XTZ&walletAddressesData={{\"coins\":{{\"XTZ\":{{\"address\":\"{address}\"}}}}}}&fiatAmount=30&fiatCurrency=USD&hideMenu=true&isFeeCalculationHidden=true&disableWalletAddressForm=true";
     }
-}
+
+     private async Task GetConnection()
+    {
+        connection = new HubConnectionBuilder()
+            .WithUrl("https://api.tzkt.io/v1/ws")
+            .WithAutomaticReconnect()
+            .Build();
+        await connection.StartAsync();
+
+         AddressObject addressObject = new AddressObject
+        {
+            addresses = new string[] { }
+        };
+
+        connection.On<object>("accounts", data => {
+            Debug.Log(data);
+        });
+
+         await connection.InvokeAsync("SubscribeToAccounts", addressObject);
+    }
+};
